5a97aae52bb6c476a9c821f7d04a91d7171052eb
Katya Civil <TheCivilAge@gmail.com>
2022-12-07 20:57:27 +0000

    Css, Javascript and html updated

---
+++ scripts/Chopple.js
@@ -0,0 +1,8 @@
+
+function openPopup(){
+    document.getElementsByClassName("popup")[0].style.display="block";
+  }
+  function closePopup(){
+    document.getElementsByClassName("popup")[0].style.display="none";
+  }
+  
